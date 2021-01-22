ab4190b93ca12289722bef1789f8fb1eb0457348
Антон Пичугин <antvlpi@gmail.com>
2021-01-22 17:29:13 +0700

    Тестирую

---
+++ AcademITSchool_Course2_AntonPichugin/Lecture0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lecture0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string welcomeString = "Hello World!";
+            Console.WriteLine(welcomeString);
+
+            Console.ReadKey();
+        }
+    }
+}
