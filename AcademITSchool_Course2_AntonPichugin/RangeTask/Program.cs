b7e85b9de96faf35a8b5ac2d175aab9ca06da8cb
Антон Пичугин <antvlpi@gmail.com>
2021-01-23 13:56:03 +0700

    RangeTask, часть 1

---
+++ AcademITSchool_Course2_AntonPichugin/RangeTask/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace RangeTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Range range1 = new Range(GetStartNumber(), GetFinalNimber());
+
+            Console.WriteLine();
+            if (range1.IsInside(GetCheckNumber()))
+            {
+                Console.WriteLine("Число лежит в диапазоне введенных чисел.");
+            }
+            else
+            {
+                Console.WriteLine("Число не лежит в диапазоне введенных чисел.");
+            }
+        }
+
+        public static double GetStartNumber()
+        {
+            Console.WriteLine("Введите первое число диапазона:");
+
+            return Convert.ToDouble(Console.ReadLine());
+        }
+
+        public static double GetFinalNimber()
+        {
+            Console.WriteLine("Введите конечно число диапазона, больше начального:");
+
+            return Convert.ToDouble(Console.ReadLine());
+        }
+
+        public static double GetCheckNumber()
+        {
+            Console.WriteLine("Введите число, чтобы проверить находится ли оно в диапазоне:");
+
+            return Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
---
+++ AcademITSchool_Course2_AntonPichugin/RangeTask/Range.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RangeTask
+{
+    class Range
+    {
+        public double From { get; set; }
+        public double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double GetLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            return From >= number && number <= To;
+        }
+    }
+}
