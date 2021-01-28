8917c8fdb50e140a8265194dc2d841e5793bde73
Антон Пичугин <antvlpi@gmail.com>
2021-01-28 21:32:37 +0700

    Three Intervals.

--- AcademITSchool_Course2_AntonPichugin/RangeTask/Program.cs
+++ AcademITSchool_Course2_AntonPichugin/RangeTask/Program.cs
@@ -6,40 +6,108 @@ namespace RangeTask
     {
         static void Main(string[] args)
         {
-            Range range1 = new Range(GetStartNumber(), GetFinalNimber());
+            const int startNumber = 1;
+            const int endNumber = 2;
 
-            Console.WriteLine();
-            if (range1.IsInside(GetCheckNumber()))
+            Range range1 = new Range(GetNumber(startNumber), GetNumber(endNumber));
+            
+            Console.WriteLine("Длина диапазона: = " + range1.GetLength());
+
+            const int checkNumber = 3;
+
+            if (range1.IsInside(GetNumber(checkNumber)))
             {
-                Console.WriteLine("Число лежит в диапазоне введенных чисел.");
+                Console.WriteLine("Введенное число принадлежит данному диапазону.");
             }
             else
             {
-                Console.WriteLine("Число не лежит в диапазоне введенных чисел.");
+                Console.WriteLine("Введенное число принадлежит данному не принадлежит диапазнону.");
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("Задайте второй диапазон.");
+            Range range2 = new Range(GetNumber(1), GetNumber(2));
+
+            // Часть2 задания
+            // Интервал-пересечение
+            Console.WriteLine();
+            Range interval_Intersection = new Range(0, 0).GetIntervalsIntersection(range1, range2);
+
+            if (interval_Intersection == null)
+            {
+                Console.WriteLine("Пересечений нет.");
+            }
+            else
+            {
+                Console.WriteLine("Новый диапазон от интервал-пересечения: от {0} до {1}.", interval_Intersection.From, interval_Intersection.To);
             }
 
-            Range range2 = new Range(GetStartNumber(), GetFinalNimber());
+            // Интервал-объединение
+            Console.WriteLine();
+            Range[] intervalsCombining = new Range(range1.From, range1.To).GetIntervalsCombining(range1, range2);
+            intervalsCombining[0] = new Range(intervalsCombining[0].From, intervalsCombining[0].To);
+
+            if (intervalsCombining.Length == 2)
+            {
+                intervalsCombining[1] = new Range(range2.From, range2.To);
+                Console.WriteLine("Объединение интервалов имеет два куска:");
+                Console.WriteLine("1 интервал: от {0} до {1}", intervalsCombining[0].From, intervalsCombining[0].To);
+                Console.WriteLine("2 интервал: от {0} до {1}", intervalsCombining[1].From, intervalsCombining[1].To);
+            }
+            else
+            {
+                Console.WriteLine("Объединение интервалов выплнено:");
+                Console.WriteLine("Новый интервал: от {0} до {1}", intervalsCombining[0].From, intervalsCombining[0].To);
+            }
+
+            //Разность интервалов
+            Console.WriteLine();
+            Range[] asymmetricalIntervalsDifference = new Range(range1.From, range1.To).GetAsymmetricalIntervalsDifference(range1, range2);
+
+            if (asymmetricalIntervalsDifference == null)
+            {
+                Console.WriteLine("Разность интервалов равно 0. Вычислить диапазон нет возможности");
+            }
+            else if (asymmetricalIntervalsDifference.Length == 2)
+            {
+                Console.WriteLine("Разность выполнена и имеет два куска:");
+                Console.WriteLine("1 интервал: от {0} до {1}", asymmetricalIntervalsDifference[0].From, asymmetricalIntervalsDifference[0].To);
+                Console.WriteLine("2 интервал: от {0} до {1}", asymmetricalIntervalsDifference[1].From, asymmetricalIntervalsDifference[1].To);
+            }
+            else
+            {
+                Console.WriteLine("Разность выполнена:");
+                Console.WriteLine("Интервал: от {0} до {1}", asymmetricalIntervalsDifference[0].From, asymmetricalIntervalsDifference[0].To);
+            }
+
+            Console.ReadKey();
         }
 
-        public static double GetStartNumber()
+        public static double GetNumber(int key)
         {
-            Console.WriteLine("Введите первое число диапазона:");
+            double number = 0.0;
 
-            return Convert.ToDouble(Console.ReadLine());
-        }
+            switch (key)
+            {
+                case 1:
+                    Console.WriteLine("Введите первое число диапазона:");
+                    number = Convert.ToDouble(Console.ReadLine());
 
-        public static double GetFinalNimber()
-        {
-            Console.WriteLine("Введите конечно число диапазона, больше начального:");
+                    break;
+                case 2:
+                    Console.WriteLine("Введите конечноe число диапазона, больше начального:");
+                    number = Convert.ToDouble(Console.ReadLine());
 
-            return Convert.ToDouble(Console.ReadLine());
-        }
+                    break;
+                case 3:
 
-        public static double GetCheckNumber()
-        {
-            Console.WriteLine("Введите число, чтобы проверить находится ли оно в диапазоне:");
+                    Console.WriteLine("Введите число, которое нужно проверить находится ли оно в диапазоне:");
+                    number = Convert.ToDouble(Console.ReadLine());
 
-            return Convert.ToDouble(Console.ReadLine());
+                    break;
+            }
+
+            return number;
         }
     }
 }
--- AcademITSchool_Course2_AntonPichugin/RangeTask/Range.cs
+++ AcademITSchool_Course2_AntonPichugin/RangeTask/Range.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace RangeTask
 {
@@ -22,7 +20,92 @@ namespace RangeTask
 
         public bool IsInside(double number)
         {
-            return From >= number && number <= To;
+            return From <= number && number <= To;
+        }
+
+        public Range GetIntervalsIntersection(Range range1, Range range2)
+        {
+            if (range1.To <= range2.From || range1.From >= range2.To)
+            {
+                return null;
+            }
+
+            if (range1.From < range2.From)
+            {
+                this.From = range2.From;
+            }
+            else
+            {
+                this.From = range1.From;
+            }
+
+            if (range1.To < range2.To)
+            {
+                this.To = range1.To;
+            }
+            else
+            {
+                this.To = range2.To;
+            }
+
+            return new Range(this.From, this.To);
+        }
+
+        public Range[] GetIntervalsCombining(Range range1, Range range2)
+        {
+            Range[] rangeArray = new Range[1];
+            rangeArray[0] = new Range(range1.From, range1.To);
+            rangeArray[0].From = range1.From;
+            rangeArray[0].To = range1.To;
+
+            if (range1.To < range2.From || range1.From > range2.To)
+            {
+                rangeArray = new Range[2];
+                rangeArray[1] = new Range(range2.From, range2.To);
+                rangeArray[1].From = range2.From;
+                rangeArray[1].To = range2.To;
+
+                return rangeArray;
+            }
+
+            if (range1.From >= range2.From)
+            {
+                rangeArray[0].From = range2.From;
+            }
+
+            if (range1.To <= range2.To)
+            {
+                rangeArray[0].To = range2.To;
+            }
+
+            return rangeArray;
+        }
+
+        public Range[] GetAsymmetricalIntervalsDifference(Range range1, Range range2)
+        {
+            Range[] rangeArray = new Range[1];
+            rangeArray[0] = new Range(range1.From, range1.To);
+            rangeArray[0].From = range1.From;
+            rangeArray[0].To = range1.To;
+
+            if (range1.To <= range2.From || range1.From >= range2.To)
+            {
+                rangeArray = new Range[2];
+                rangeArray[1] = new Range(range2.From, range2.To);
+                rangeArray[1].From = range2.From;
+                rangeArray[1].To = range2.To;
+
+                return rangeArray;
+            }
+
+            if (range1.From == range2.From && range1.To == range2.To)
+            {
+                return null;
+            }
+
+            rangeArray[0].To = range2.From;
+
+            return rangeArray;
         }
     }
 }
